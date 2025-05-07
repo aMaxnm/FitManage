16198143a49ed2630efc6163f5151a21cbb05529
Maximiliano Núñez <antonio.nunez25@uabc.edu.mx>
2025-05-07 13:21:59 -0700

    PBI-US-MEM1
    Se agrego la clase ValidarDatos a la capa negocio

---
+++ FitManage/Negocio/ValidarDatos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class ValidarDatos
+    {
+            public static bool ValidarTexto(string texto)
+            {
+                return !string.IsNullOrWhiteSpace(texto) && System.Text.RegularExpressions.Regex.IsMatch(texto, "^[a-zA-ZáéíóúÁÉÍÓÚñÑ]+$");
+            }
+
+            public static bool ValidarTelefono(string telefono)
+            {
+                return !string.IsNullOrWhiteSpace(telefono) && System.Text.RegularExpressions.Regex.IsMatch(telefono, "^[0-9]+$");
+            }
+
+            public static bool ValidarFecha(DateTime fechaSeleccionada)
+            {
+                return fechaSeleccionada.Date >= DateTime.Now.Date;
+            }
+
+            public static bool ValidarImagen(object imagen)
+            {
+                return imagen != null;
+            }
+    }
+}
