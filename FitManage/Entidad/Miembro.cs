d401d7ddbd4c9cccabdd96868f325bbbc942afc6
Ramiro Pimentel <ramiro.pimentel@uabc.edu.mx>
2025-05-07 22:23:01 -0700

    PBI-US Entidad
    Entidad y miembro

---
+++ FitManage/Entidad/Miembro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    internal class Miembro
+    {
+        public int IdMiembro { get; set; }
+        public int IdMembresia { get; set; }
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string NumeroCelular { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public byte[] Foto { get; set; }
+    }
+}
