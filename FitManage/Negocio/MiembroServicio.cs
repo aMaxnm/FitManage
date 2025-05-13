a5d979e97fbeceb20cb16447f9794c471641d33a
Ramiro Pimentel <ramiro.pimentel@uabc.edu.mx>
2025-05-12 23:48:43 -0700

    PBI-US-MEM4 Edicion de Miembro
    Edición en la interfaz y base de datos

--- FitManage/Negocio/MiembroServicio.cs
+++ FitManage/Negocio/MiembroServicio.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using AccesoDatos;
 using Entidad;
@@ -15,5 +16,23 @@ namespace Negocio
         {
             return miembroDAO.ObtenerMiembros();
         }
+
+        public void Actualizar(Miembro miembro)
+        {
+            // Validaciones de negocio
+            if (string.IsNullOrWhiteSpace(miembro.Nombres))
+                throw new ArgumentException("El nombre no puede estar vacío.");
+
+            if (string.IsNullOrWhiteSpace(miembro.NumeroTelefono))
+                throw new ArgumentException("El número telefónico no puede estar vacío.");
+
+            if (!Regex.IsMatch(miembro.NumeroTelefono, @"^\d{10}$"))
+                throw new ArgumentException("El número telefónico debe tener 10 dígitos.");
+
+            // Llama al DAO para hacer el update real
+            miembroDAO.Actualizar(miembro);
+        }
+
     }
+
 }
