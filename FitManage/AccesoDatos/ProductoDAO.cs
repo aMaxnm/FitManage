584e460f2059c621bafc35036457675d8a81121b
Ramiro Pimentel <ramiro.pimentel@uabc.edu.mx>
2025-05-24 17:47:29 -0700

    PBI-US-INV1 Producto Panel
    Panel y Consulta

---
+++ FitManage/AccesoDatos/ProductoDAO.cs
@@ -0,0 +1,41 @@
+﻿// Capa AccesoDatos
+using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using Entidad;
+
+namespace AccesoDatos
+{
+    public class ProductoDAO
+    {
+        private string connectionString = "server=localhost;port=8000;user=root;password=root;database=fitmanage;";
+
+        public List<Producto> ObtenerProductos()
+        {
+            List<Producto> productos = new List<Producto>();
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT * FROM productos";
+
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        productos.Add(new Producto
+                        {
+                            IdProducto = reader.GetInt32("Id_producto"),
+                            Nombre = reader.GetString("Nom_producto"),
+                            Descripcion = reader.GetString("Descripcion"),
+                            Cantidad = reader.GetInt32("Cantidad"),
+                            Precio = reader.GetDecimal("Precio")
+                        });
+                    }
+                }
+            }
+            return productos;
+        }
+    }
+}
